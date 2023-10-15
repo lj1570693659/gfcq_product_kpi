96d3de525009bdcabb78970e57770cb9717a6581
unknown <1570693659@qq.com>
2023-10-15 17:36:57 +0800

    fix:core

--- build_linux.bat
+++ build_linux.bat
@@ -11,6 +11,3 @@ echo now the GOARCH:
 go env GOARCH
 
 go build -o main main.go
-
-@REM docker build -t wxmsg_all -f .\Dockerfile .
-@REM docker run -itd -p 8089:8089  --name wxmsg_all d54c7370f99d99a149d9e7c77e897859bd26e304e7b35fa115b7d4b3cc0953c3
